3191098d5d83a2f8c2b54a5cb0cd6eeb027bac86
chucklapress <charleslapress@gmail.com>
2020-04-11 18:11:02 -0400

    cleaned up writes added foreground colors and comments

--- zipcodeCheck.ps1
+++ zipcodeCheck.ps1
@@ -1,9 +1,16 @@
 #PS script to check canadian postal codes
+
 Write-Host "Welcome to my little script" `n
 $postal = read-host "what is your postal code? ..."
-if ($postal -match '[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]') {
-write-host "Looks like you are from the land of the ice and snow"
-} ElseIf ($postal -match "^[0-9]{5}(?:-[0-9]{4})?$") {
-write-host "Gessing you reside state side partner" 
-} else {
-write-host "You aren't from around these parts are you?" }
+
+# Canadian postal codes can't contain the letters D, F, I, O, Q, or U, and cannot start with W or Z
+If ($postal -match '[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]') {
+  write-host "Oh Canada, My home and native land" -fore green
+}
+# verifies zipcode and zip plus 4
+ElseIf ($postal -match "^[0-9]{5}(?:-[0-9]{4})?$") {
+  write-host "Red, White and Blue baby" -fore blue
+}
+# Doesn't follow either regex pattern
+Else {
+write-host "You aren't from around these parts are you?" -fore red}
