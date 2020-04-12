e88219499b5c1a08adcf0892746c6f9a1d8c2eca
chucklapress <charleslapress@gmail.com>
2020-04-12 13:55:45 -0400

    added to upper

--- zipcodeCheck.ps1
+++ zipcodeCheck.ps1
@@ -2,6 +2,8 @@
 
 Write-Host "Welcome to my little script" `n
 $postal = read-host "what is your postal code? ..."
+# Allow for the processing of a user entering data in lower case to process
+$postal = $postal.ToUpper()
 
 # Canadian postal codes can't contain the letters D, F, I, O, Q, or U, and cannot start with W or Z
 If ($postal -match '[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]') {
